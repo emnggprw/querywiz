f76db83bad81eae74a1e859ef790b881ac967ce1
Eko <eko.mnggprw@gmail.com>
2025-03-18 00:08:39 +0700

    Project files refactor for better maintenance

---
+++ lib/querywiz_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:querywiz/screens/chat_screen.dart';
+
+class QueryWizApp extends StatelessWidget {
+  const QueryWizApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.cyan),
+        scaffoldBackgroundColor: Colors.black,
+      ),
+      home: const ChatScreen(),
+    );
+  }
+}
